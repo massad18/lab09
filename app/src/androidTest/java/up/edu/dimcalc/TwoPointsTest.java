d86729d31ece5dff3feeb2a5ef693bbcab610726
massad18 <massad18@up.edu>
2016-11-09 15:38:37 -0800

    CP1

---
+++ app/src/androidTest/java/up/edu/dimcalc/TwoPointsTest.java
@@ -0,0 +1,37 @@
+package up.edu.dimcalc;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by massad18 on 11/9/2016.
+ */
+public class TwoPointsTest extends TestCase {
+
+    public void testGetPoint() throws Exception {
+
+    }
+
+    public void testSetPoint() throws Exception {
+
+    }
+
+    public void testRandomValue() throws Exception {
+
+    }
+
+    public void testSetOrigin() throws Exception {
+
+    }
+
+    public void testCopy() throws Exception {
+
+    }
+
+    public void testDistance() throws Exception {
+
+    }
+
+    public void testSlope() throws Exception {
+
+    }
+}
